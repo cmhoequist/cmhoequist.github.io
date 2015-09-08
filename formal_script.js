a53c049374c98e7e690ec0765fbe2d417bc3fc44
cmhoequist <cmhoequist@gmail.com>
2015-09-08 12:44:47 -0500

    Update formal_script.js

--- formal_script.js
+++ formal_script.js
@@ -1,12 +1,12 @@
-$(window).scroll(function(){
-  var win = $(window);
-  var scrollVal = win.scrollTop();
-  if(scrollVal>=10){
-    $(".navbar-menu").css("border-bottom","2px solid #888888");
-    $(".navbar-menu").css("background","#f3f0da");
-  }
-  else{
-    $(".navbar-menu").css("border-bottom","none");
-    $(".navbar-menu").css("background","white");
-  }
+$(document).ready(function(){
+  $('#me').hover(
+    function(){
+      $(this).text("Moritz Hoequist");
+      $(this).width('150');
+    },
+    function(){
+      $(this).text("M");
+      $(this).width('auto');
+    }
+  );
 });
