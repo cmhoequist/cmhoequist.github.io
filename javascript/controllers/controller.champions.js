9ebc99be598477e00650f667e310e5b5c6695aa3
Moritz <cmhoequist@gmail.com>
2017-04-15 00:50:35 -0500

    added ng-src

--- javascript/controllers/controller.champions.js
+++ javascript/controllers/controller.champions.js
@@ -1,12 +1,15 @@
 (function(){
   angular.module('champApp').controller('ChampCtrl', ChampCtrl);
 
-  function ChampCtrl($scope){
+  function ChampCtrl($scope, Search){
     var self = this;
     self.name = "YOU CAN'T MILK THOSE";
 
-    self.update = function(name, Search, Draw){
+    self.update = function(name){
+      console.log(name);
+      console.log('http://ddragon.leagueoflegends.com/cdn/img/champion/splash/'+name+'_0.jpg');
         self.name = name;
+        self.champion = name;
         self.image = Search.request(name);
         //call searching service
     }
